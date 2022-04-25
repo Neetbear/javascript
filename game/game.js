d4ee5b08baa1faaa0c6015404c4830d7afca366e
Neetbear <52730691+Neetbear@users.noreply.github.com>
2022-04-25 12:20:15 +0900

    0425
    탈출게임 - 몬스터 가위바위보 진행 중

--- game/game.js
+++ game/game.js
@@ -1,20 +1,27 @@
 const canvas = document.getElementById("myCanvas");
 const context = canvas.getContext("2d");
 
+document.addEventListener('keydown', keyDownEventHandler);
+
 const fieldWidth = 50;
 const fieldHeight = 50;
 const fieldColumn = 10;
 const fieldRow = 10;
+let userPosX = 50;
+let userPosY = 50;
+const arcRadius = 25
+let gameState = 0;
 
 const fieldType = ["isMonster", "isShop", "justField"];
 class Field {
-    constructor(left, top, right, bottom, column, row, isMonster) {
+    constructor(left, top, right, bottom, column, row, fieldStyle, isMonster) {
         this.left = left;
         this.top = top;
         this.right = right;
         this.bottom = bottom;
         this.column = column;
         this.row = row;
+        this.fieldStyle = fieldStyle;
         this.isMonster = isMonster;
     }
 }
@@ -24,18 +31,89 @@ class User extends Field {
         this.money = money;
         this.HP = HP;
     };
-
 }
 class Gate extends Field {
     constructor(left, top, right, bottom){
         super(left, top, right, bottom);
     };
 }
-
 class Shop extends Field {
     constructor(left, top, right, bottom){
         super(left, top, right, bottom);
     };
+}
+
+let user = [];
+function setUser() {
+    user = new User(
+        userPosX, 
+        userPosY, 
+        userPosX + fieldWidth, 
+        userPosY + fieldHeight,
+        100, 0
+    )
+}
+function userDraw() {
+    setUser();
+    context.beginPath();
+    context.arc(user.left + fieldWidth / 2, user.top + fieldHeight / 2, arcRadius, 0, 2 * Math.PI);
+    context.fillStyle = "rgb(214, 54, 54)"; 
+    context.fill(); 
+    context.closePath();
+}
+
+let gate = [];
+function setGate() {
+    let overlap = true;
+    let i, j;
+    while(overlap) {
+        i = Math.floor(Math.random() * 9);
+        j = Math.floor(Math.random() * 9);
+        if((i == 0) && (j == 0)) { 
+            overlap = true;
+            continue;
+        } else { 
+            overlap = false;
+            gate = new Gate(
+                fieldWidth * (i + 1), 
+                fieldHeight * (j + 1) , 
+                fieldWidth * (i + 2), 
+                fieldHeight * (j + 2)
+            )
+        }
+    }
+}
+setGate();
+function gateDraw() {
+    context.beginPath();
+    context.arc(gate.left + fieldWidth / 2, gate.top + fieldHeight / 2, arcRadius, 0, 2 * Math.PI);
+    context.fillStyle = "black"; 
+    context.fill(); 
+    context.closePath();
+}
+
+function keyDownEventHandler(e) {
+    if(e.key == "ArrowRight") {
+        console.log("ArrowRight")
+        if(userPosX < canvas.width -100) {
+            userPosX += fieldWidth;
+        }
+    } else if(e.key == "ArrowLeft") {
+        console.log("ArrowLeft")
+        if(userPosX > fieldWidth) {
+            userPosX -= fieldWidth;
+        }
+    } else if(e.key == "ArrowUp") {
+        console.log("ArrowUp")
+        if(userPosY > fieldHeight) {
+            userPosY -= fieldHeight;
+        }
+    } else if(e.key == "ArrowDown") {
+        console.log("ArrowDown")
+        if(userPosY < canvas.height -100) {
+            userPosY += fieldHeight;
+        }
+    }
 }
 
 let fields = [];
@@ -48,11 +126,12 @@ function setFields() {
                 50 + i * fieldHeight, 
                 50 + (j + 1) * fieldWidth, 
                 50 + (i + 1) * fieldHeight,
-                i, j, Math.round(Math.random())
+                i, j, Math.round(Math.random() * 3), Math.round(Math.random())
             );
         }
     } 
 }
+setFields();
 
 const fieldDesign = ["forest", "river", "desert", "snow"];
 const fieldsColor = ["lightgreen", "lightblue", "bisque", "whitesmoke"]; // 숲 강 사막 
@@ -61,20 +140,44 @@ function drawFields() {
         for(let j = 0; j < fieldColumn; j++) {
             context.beginPath();
             context.rect(fields[i][j].left, fields[i][j].top, fieldWidth, fieldHeight);
-            context.fillStyle = fieldsColor[(i%4)]; 
+            context.fillStyle = fieldsColor[fields[i][j].fieldStyle]; 
             context.fill(); 
             context.closePath();
         }
     }
 }
 
+function gameClear() {
+    if((userPosX == gate.left) && (userPosY ==  gate.top)) {
+        gameState = 2;
+        setTimeout(() => { alert("Game Clear") }, 100);
+    }
+}
+
+
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height); 
     drawFields();
+    userDraw();
+    // gateDraw();
 }
-function updata () {
 
+function meetMonster() {
+    // console.log((userPosX-50)/50, (userPosY-50)/50);
+    if(fields[(userPosX-50)/50][(userPosY-50)/50].isMonster) {
+        console.log("meet monster");
+        gameState = 1;
+    }
 }
-draw();
-// setInterval(draw, 10);
-// setInterval(updata, 10);
+
+function fightMonster() {
+    
+}
+
+function updata () {
+    meetMonster();
+    gameClear();
+}
+
+setInterval(() => {if(gameState == 0) {draw()}}, 10);
+setInterval(() => {if(gameState == 0) {updata()}}, 10);
